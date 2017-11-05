f5bd1a3a40ec80ec81b829a5db2b506855f6d8be
Myth52125 <myth52125@hotmail.com>
2017-11-05 15:10:19 +0800

    100%

--- stl_v3.3/stl_construct.h
+++ stl_v3.3/stl_construct.h
@@ -43,6 +43,32 @@ __STL_BEGIN_NAMESPACE
 
 // Internal names
 
+
+// http://www.cnblogs.com/luxiaoxun/archive/2012/08/10/2631812.html
+// new ：不能被重载，其行为总是一致的。
+// 它先调用operator new分配内存，然后调用构造函数初始化那段内存。
+
+// new 操作符的执行过程：
+// 1. 调用operator new分配内存 ；
+// 2. 调用构造函数生成类对象；
+// 3. 返回相应指针。
+
+// （2）operator new：要实现不同的内存分配行为，应该重载operator new，而不是new。
+
+// operator new就像operator + 一样，是可以重载的。
+// 如果类中没有重载operator new，那么调用的就是全局的::operator new来完成堆的分配。
+// 同理，operator new[]、operator delete、operator delete[]也是可以重载的。
+
+// （3）placement new：只是operator new重载的一个版本。它并不分配内存，
+// 只是返回指向已经分配好的某段内存的一个指针。
+// 因此不能删除它，但需要调用对象的析构函数。
+
+// 如果你想在已经分配的内存中创建一个对象，使用new时行不通的。
+// 也就是说placement new允许你在一个已经分配好的内存中（栈或者堆中）构造一个新的对象。
+// 原型中void* p实际上就是指向一个已经分配好的内存缓冲区的的首地址。
+
+
+// placement new
 template <class _T1, class _T2>
 inline void _Construct(_T1* __p, const _T2& __value) {
   new ((void*) __p) _T1(__value);
@@ -56,11 +82,15 @@ inline void _Construct(_T1* __p) {
   new ((void*) __p) _T1();
 }
 
+// placement new
+// 需要直接调用析构函数去析构.
+// 因为内存空间不是placement new分配的,所以不建议使用delete释放空间.
 template <class _Tp>
 inline void _Destroy(_Tp* __pointer) {
   __pointer->~_Tp();
 }
 
+// 批量释放
 template <class _ForwardIterator>
 void
 __destroy_aux(_ForwardIterator __first, _ForwardIterator __last, __false_type)
@@ -86,6 +116,7 @@ inline void _Destroy(_ForwardIterator __first, _ForwardIterator __last) {
   __destroy(__first, __last, __VALUE_TYPE(__first));
 }
 
+// 特例化
 inline void _Destroy(char*, char*) {}
 inline void _Destroy(int*, int*) {}
 inline void _Destroy(long*, long*) {}
